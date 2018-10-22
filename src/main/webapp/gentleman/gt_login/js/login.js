ecccc2abc237560b4aaa6e29c7417604eb5fa01a
fujianming1996 <https://github.com/fujianming1996/gentleman.git>
2018-10-22 16:43:24 +0800

    登录

--- src/main/webapp/gentleman/gt_login/js/login.js
+++ src/main/webapp/gentleman/gt_login/js/login.js
@@ -20,7 +20,21 @@ var login = {
                 delCookie("username");
                 console.log(getCookie("username"))
             }
-        })
+        });
+
+		$(".login_way_left").mouseover(function () {
+            $(".login_way_left").css('background','#8B8682');
+        });
+        $(".login_way_left").mouseleave(function () {
+            $(".login_way_left").css('background','#CDC5BF');
+        });
+        $(".login_way_right").mouseover(function () {
+            $(".login_way_right").css('background','#8B8682');
+        });
+        $(".login_way_right").mouseleave(function () {
+            $(".login_way_right").css('background','#CDC5BF');
+        });
+
 	}
 };
 
